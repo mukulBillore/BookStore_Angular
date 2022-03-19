fceb297535a910366340498ea3f3b09a9d4276f7
mukulBillore <mukulbillore88@gmail.com>
2022-03-19 23:14:52 +0530

    Initial Commit :added the book store application  initial stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './MyComponent/login/login.component';
+import { RegisterComponent } from './MyComponent/register/register.component';
+import {  HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './MyComponent/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
