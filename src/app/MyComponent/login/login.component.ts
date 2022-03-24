f4cef7a62de225c2e9013b745794753f555dec69
mukulBillore <mukulbillore88@gmail.com>
2022-03-24 12:30:09 +0530

    test commit

--- src/app/MyComponent/login/login.component.ts
+++ src/app/MyComponent/login/login.component.ts
@@ -17,52 +17,71 @@ export class LoginComponent implements OnInit {
 
   // To store user enterd password for login
   password!:string;
-  
+
+  v1:string="1";
+  v2:number=12
+  v3!:number;  
+
   // To store the status of login weather sucussfull or wrong email or wrong password
   status:any;
 
   // to store the logined in user user id 
-  userId!:any;
+  userId!:any
+  tostring!:string
 
   ngOnInit(): void {
+    this.userService.loginUser("mukulbilllore88@gmail.com","123").subscribe((getData:any)=>{
+      console.log(getData);
+      this.userId=this.userService.getUserId(this.email);
+      this.tostring=this.userId.tostring();
+      console.log("the type of ",this.tostring," i s ",typeof(this.tostring))
+
+    });
   }
 
   // On Submit checks whether the login credentials are correct or not if correct then redirects to home page
   onClickSubmit(){
+    
     this.userService.loginUser(this.email,this.password).subscribe((getData:any)=>{
       console.log(getData);
       this.status=getData;
     });
     this.router.navigate(["home"]);
+    
   }
+
 
   submitTestLoginStatus(){
     this.userService.getloginStatus(this.email,this.password).subscribe((getData:any)=>{
       console.log(getData);
       this.status=getData;
-
     });
       if(this.status==1){
         
         this.userService.loginUser(this.email,this.password).subscribe((getData:any)=>{
           console.log(getData);
           this.userId=this.userService.getUserId(this.email);
+          this.tostring=this.userId.tostring();
+          console.log("the type of ",this.tostring," i s ",typeof(this.tostring))
     
         });
-        this.router.navigate(["home",this.userId]);
-        console.log("login sucussfull");
+        //this.router.navigate(["home",this.userId]);
+        this.router.navigate(["home"]);
         alert("Login sucussfull");
-      }
-      else if(this.status==0){
+        console.log("login sucussfull");
+
+        //window.location.reload()
+        }
+       if(this.status==0){
         console.log("invalid user email");
         //alert("wrong email");
       }   
-      else if(this.status==2) {
+       if(this.status==2) {
         console.log("invalid user password");
         //alert("wrong password");        
       } 
     
-   // this.router.navigate(["login"]);
+      
   }
 
   onClickRedirectToRegister(){
